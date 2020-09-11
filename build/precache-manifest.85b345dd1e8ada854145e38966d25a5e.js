8cfb983175ff6bb8ab147b840eea66d7b0c6f779
piyushsi <piyushsinha2309@gmail.com>
2020-09-12 04:42:25 +0530

    gh-pages

---
+++ build/precache-manifest.85b345dd1e8ada854145e38966d25a5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3e07db99cf952b593e233acb7c8562c",
+    "url": "/Dev-Friendly-Canvanizer/index.html"
+  },
+  {
+    "revision": "0490295b1ae3b59f68a0",
+    "url": "/Dev-Friendly-Canvanizer/static/css/main.53564032.chunk.css"
+  },
+  {
+    "revision": "606eb86a0392169ab193",
+    "url": "/Dev-Friendly-Canvanizer/static/js/2.662f72e9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Dev-Friendly-Canvanizer/static/js/2.662f72e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0490295b1ae3b59f68a0",
+    "url": "/Dev-Friendly-Canvanizer/static/js/main.a9e47e7e.chunk.js"
+  },
+  {
+    "revision": "6da218a693e946ff7b31",
+    "url": "/Dev-Friendly-Canvanizer/static/js/runtime-main.1057c131.js"
+  }
+]);
